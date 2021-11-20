7c3bb853f2e45c1f523df8f5adc797bf358f219f
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-19 21:01:35 -0600

    Create Funcion4.m

---
+++ Funcion4.m
@@ -0,0 +1,19 @@
+% Octave Script
+% Title         :Funcion 2
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion algebraica
+% Date          :20211115
+% Version       :1
+% Usage         :octave>cd
+%               :
+%               :Requiere de octave
+
+clear
+pkg load symbolic
+syms x
+x=[-5:1:5];
+fx= ((2.*x)+(1));
+plot (x, fx);
+title (['La funcion 4 es polinominal']);
+disp ('Es una funcion polinominal');
+disp ('f(x)=0 cuando x=-0.5');
