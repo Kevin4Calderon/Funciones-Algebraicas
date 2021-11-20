7a7ad49610699122da16071d6b681411c531bc1e
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-19 20:57:34 -0600

    Create Funcion3.m

---
+++ Funcion3.m
@@ -0,0 +1,18 @@
+% Octave Script
+% Title         :Funcion 2
+% Author        :Kevin Yoan Calderón García
+% Description   :Script para graficar una funcion algebraica
+% Date          :20211115
+% Version       :1
+% Usage         :octave>cd
+%               :
+%               :Requiere de octave
+
+clear
+pkg load symbolic
+syms x
+x = [-10:1:10];
+fx= (x+2)./(x-1);
+plot (x, fx);
+title (['La funcion 3 es racional']);
+disp ("es una funcion racional");
